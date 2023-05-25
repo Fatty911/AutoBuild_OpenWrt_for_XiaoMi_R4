2fcf885e4152477a45c011d6a9a515f526b96bc0
Fatty911 <109130780+Fatty911@users.noreply.github.com>
2023-05-25 13:14:59 +0800

    add OpenClash

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
