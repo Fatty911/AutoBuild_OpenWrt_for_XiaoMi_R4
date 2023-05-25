5174c85fe3cdca80217e6ba332b5063c7f22425e
Fatty911 <109130780+Fatty911@users.noreply.github.com>
2023-05-25 13:20:06 +0800

    add Kenzok8/small-package

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
