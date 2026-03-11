9027fb3bd93d7f4912bb6268490b2513f9901682
Fatty911 <xuerui911@gmail.com>
2026-03-11 15:59:10 +0800

    禁用passwall2 feed源
    解决feeds/passwall2目录不存在导致make defconfig失败

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 #echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
